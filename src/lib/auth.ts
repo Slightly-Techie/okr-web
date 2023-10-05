c931c1d5853944a9ad67f48064ef91201cfb0ead
Diabene <addodiabene69@gmail.com>
2023-10-05 18:33:12 +0000

    fix abort error

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -9,8 +9,8 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID as string,
-      clientSecret: process.env.GOOGLE_SECRET as string,
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
       authorization: {
         params: {
           prompt: "consent",
