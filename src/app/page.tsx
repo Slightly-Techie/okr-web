df453a02d6eff6aff1b3b02a5a55c113bb3e7926
Diabene <addodiabene69@gmail.com>
2023-10-05 16:41:12 +0000

    fix conflicts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
-'use client';
+"use client";
 import { signIn } from "next-auth/react";
-
 
 export default function Home() {
   return (
@@ -8,7 +7,6 @@ export default function Home() {
       <h1 className=" text-6xl text-slate-50 font-bold">
         Welcome to the OKR Project
       </h1>
-      <button className="bg-white rounded-xl px-3 py-3 text-green-700 absolute top-[65%] left-[50%] translate-x-[-50%] translate-y-[-50%]" onClick={() => signIn('google')}>sign in with google</button>
     </main>
   );
 }
